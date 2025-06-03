fe578bb9a94f9377bf71ef0804e927a9cc8f14a0
naderZ2 <{ID}+{username}@users.noreply.github.com>
2025-06-03 03:26:54 +0300

    procfile11

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def predict():
         return jsonify({'error': f'Missing feature: {e}'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
